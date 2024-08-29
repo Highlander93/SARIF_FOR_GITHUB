c108672980e2b065b9ccbaec5027bed5e325173e
Müller <mueller-steven@hotmail.de>
2024-08-29 13:32:26 +0200

    Create SARIF_FOR_GITHUB.c

---
+++ SARIF_FOR_GITHUB.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int number0 = 0;
+   int number1 = 1;
+   int numberFinal;
+   numberFinal = number1 / number0;
+   return 0;
+}
